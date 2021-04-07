982aa59ce0300ed18f559d0035209e34fdcedf84
Robert Maury <daquinox@gmail.com>
2021-04-07 14:32:28 -0700

    Update fervid.pp

--- site-modules/role/manifests/fervid.pp
+++ site-modules/role/manifests/fervid.pp
@@ -3,7 +3,7 @@ class role::fervid {
   class {'::secure_linux_cis':
    time_servers  => ['us.pool.ntp.org'],
    profile_type  => 'server',
-   allow_users   => 'robert',
+   allow_users   => ['robert'],
   }
 
 }
